57866a9ca5f8e97ff55e9736d72d1cb794e711da
EataPanini <74978921+EataPanini@users.noreply.github.com>
2021-10-21 11:39:41 -0700

    updated batstatus.sh and made it diffeence thinf

--- batstatus.sh
+++ batstatus.sh
@@ -4,23 +4,28 @@
 BATDIR=`ls /sys/class/power_supply/ | grep BAT | cat`
 echo $BATDIR
 
+#for some un godly reason the current charge value name is different for some laptop brands, WHY WHY THE AJKDNAWKJDNKJAWDN
+CHRG_NOW=`ls /sys/class/power_supply/$BATDIR | grep 'energy\|charge' | sed -n 3,1p`
+CHRG_FULL=`ls /sys/class/power_supply/$BATDIR | grep 'energy\|charge' | sed -n 1,1p`
+
+echo $CHRG_NOW
+echo $CHRG_FULL
+
 #get the current battery charge and compare it with old one
 while :
 	do
 	#get last battery reading
-	BAT_LAST=`cat /sys/class/power_supply/$BATDIR/charge_now`
+	BAT_LAST=`cat /sys/class/power_supply/$BATDIR/$CHRG_NOW`
 
 	sleep 5
 
 	#get current battery reading
-	BAT_CURRENT=`cat /sys/class/power_supply/$BATDIR/charge_now`
+	BAT_CURRENT=`cat /sys/class/power_supply/$BATDIR/$CHRG_NOW`
 
 	#print the last and current measurements (for debugging)
 	echo $BAT_LAST
 	echo $BAT_CURRENT
 
 	#subtract BAT_LAST from BAT_CURRENT to get the battery usage
-	$BAT_LAST - 
+	#$BAT_LAST - 
 done
-
-
